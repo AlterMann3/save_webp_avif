018169652060152c9a20e79a8be1b2fc37870b29
AlterMann3 <118202564+AlterMann3@users.noreply.github.com>
2026-03-13 23:43:53 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -5,21 +5,6 @@
 @description: Custom node to save your images in WebP or AVIF.
 """
 
-
 from .save_webp_avif import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-WEB_DIRECTORY = "./web"
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', "WEB_DIRECTORY"]
-
-from aiohttp import web
-from server import PromptServer
-from pathlib import Path
-
-if hasattr(PromptServer, "instance"):
-  # NOTE: we add an extra static path to avoid comfy mechanism
-  # that loads every script in web.
-  PromptServer.instance.app.add_routes(
-      [web.static("/save_webp_avif", (Path(__file__).parent.absolute() / "assets").as_posix())]
-  )
-
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
